fbcc92fd0408d5e45899b07cd012597c00940049
Angie Barreto <angiepbg13@gmail.com>
2025-03-06 22:10:41 -0500

    Add promise handling with Promise.all, Promise.allSettled and Promise.any.

--- asincronia/indexA.js
+++ asincronia/indexA.js
@@ -42,7 +42,7 @@ pedirComida
     console.error(error); // Si hubo un error
   });
   
-*/
+
 function hacerPedidoPizza() {
   return new Promise((resolve, reject) => {
     let ingredientes = Math.random() > 0.5;
@@ -68,4 +68,141 @@ async function hacerPedido() {
 }
 
 hacerPedido();
+*/
 
+function hacerPedidoPizza() {
+  return new Promise((resolve, reject) => {
+    let ingredientes = Math.random() > 0.3;
+    setTimeout(() => {
+      if (ingredientes) {
+        resolve("🍕 Pedido listo: Pizza.");
+      } else {
+        reject("❌ No hay ingredientes para la pizza.");
+      }
+    }, 4000);
+  });
+}
+
+function hacerPedidoHamburguesa() {
+  return new Promise((resolve, reject) => {
+    let ingredientes = Math.random() > 0.3;
+    setTimeout(() => {
+      if (ingredientes) {
+        resolve("🍔 Pedido listo: Hamburguesa.");
+      } else {
+        reject("❌ No hay ingredientes disponibles.");
+      }
+    }, 3000);
+  });
+}
+
+function hacerPedidoTacos() {
+  return new Promise((resolve, reject) => {
+    let ingredientes = Math.random() > 0.3;
+    setTimeout(() => {
+      if (ingredientes) {
+        resolve("🌮 Pedido listo: Tacos.");
+      } else {
+        reject("❌ No hay ingredientes para los tacos.");
+      }
+    }, 5000);
+  });
+}
+/* promise.all()
+async function hacerPedidos() {
+  try {
+    console.log("⏳ Esperando pedidos...");
+    let resultados = await Promise.all([
+      hacerPedidoPizza(),
+      hacerPedidoHamburguesa(),
+      hacerPedidoTacos(),
+    ]);
+    console.log("✅ Todos los pedidos están listos:");
+    console.log(resultados);
+  } catch (error) {
+    console.error("❌ Hubo un problema con algún pedido:", error);
+  }
+}
+
+
+async function hacerPedidos() {
+  console.log("⏳ Esperando pedidos...");
+
+  let resultados = await Promise.allSettled([
+    hacerPedidoPizza(),
+    hacerPedidoHamburguesa(),
+    hacerPedidoTacos(),
+  ]);
+
+  console.log("📦 Resultados de los pedidos:");
+  resultados.forEach((resultado) => {
+    if (resultado.status === "fulfilled") {
+      console.log("✅", resultado.value);
+    } else {
+      console.error("❌", resultado.reason);
+    }
+  });
+}
+
+hacerPedidos();
+
+
+async function obtenerUsuario(id) {
+  if (id < 1 || id > 10) {
+    throw new Error("⚠️ El ID debe estar entre 1 y 10.");
+  }
+
+  try {
+    let respuesta = await fetch(
+      `https://jsonplaceholder.typicode.com/users/${id}`
+    );
+
+    if (!respuesta.ok) {
+      throw new Error("❌ Error en la respuesta del servidor.");
+    }
+
+    let usuario = await respuesta.json();
+    console.log(
+      "✅ Datos del usuario son correctos:",
+      usuario.name,
+      "-",
+      usuario.email
+    );
+  } catch (error) {
+    console.error("⚠️ Ocurrió un error:", error.message);
+  }
+}
+
+obtenerUsuario(3);
+*/
+
+function falloElRepartidor(nombre, tiempo) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject(`❌ Fallo el repartidor ${nombre}`);
+    }, tiempo);
+  });
+}
+
+function exitoRepartidor(nombre, tiempo) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(`✅ Pedido de ${nombre} entregado en ${tiempo}ms`);
+    }, tiempo);
+  });
+}
+
+async function primerRepartidorExitoso() {
+  try {
+    let resultado = await Promise.any([
+      falloElRepartidor("Repartidor1", 1000),
+      exitoRepartidor("Repartidor2", 2000),
+      falloElRepartidor("Repartidor3", 1500),
+    ]);
+    console.log("🚀 Primer repartidos exitoso:", resultado);
+  } catch (error) {
+    console.error("⚠️ Ningún repartidor tuvo éxito", error);
+  }
+}
+
+primerRepartidorExitoso();
